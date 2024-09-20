65f53b077043e2e47633780e4595cfef6b9cd515
Mathis FOUREL <mathis.fourel@epfedu.fr>
2024-09-20 16:31:41 +0200

    Création d'un nouveau projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -4,6 +4,8 @@
  */
 package exo2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author foure
@@ -16,21 +18,25 @@ public class Exo2 {
     public static void main(String[] args) {
        
         //Declaration des variables
-int nb = 0; // nombre d'entiers a additionner
-int result; // resultat
-int ind; //indice
-nb=5;
-result=0;
+        int nb; // nombre d'entiers a additionner
+        int result; // resultat
+        int ind; //indice
 
+//nb=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+
+        result=0;
 // Additionner des nb premiers entier
-ind=1;
-while (ind <= nb)  {
-    result=result+ind;
-}
-
+        ind=1;
+        while (ind <= nb)  {
+        result=result+ind;
+        ind+=1;
+        }
 // Afficher du resultat
-System.out.println();
-System.out.println("La somme des "+ nb + "entiers est "+result);
+        System.out.println();
+        System.out.println("La somme des "+ nb + " entiers est "+result);
     }
 }
     
