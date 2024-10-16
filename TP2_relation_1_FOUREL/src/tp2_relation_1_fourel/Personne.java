bc0dd4fc6b1741a23c2905cf20dc0754501f90dd
Mathis FOUREL <mathis.fourel@epfedu.fr>
2024-10-16 11:06:58 +0200

    Création d'un nouveau projet versionné

--- TP2_relation_1_FOUREL/src/tp2_relation_1_fourel/Personne.java
+++ TP2_relation_1_FOUREL/src/tp2_relation_1_fourel/Personne.java
@@ -10,5 +10,33 @@ package tp2_relation_1_fourel;
  * @author foure
  */
 public class Personne {
+    String nom;
+    String prenom;
+    int nbVoitures;
+    Voiture[] liste_voitures = new Voiture[3] ;
+
+    public Personne(String nomPersonne, String prenomPersonne) {
+        nom = nomPersonne;
+        prenom = nomPersonne; 
+        
+    }
     
+        public boolean ajouter_voiture (Voiture voiture_a_ajouter) {
+        if (voiture_a_ajouter.Proprietaire != null) {
+            return false;
+        }
+         if (nbVoitures >= 3) {
+            return false;
+        }
+        liste_voitures[nbVoitures] = voiture_a_ajouter;
+        nbVoitures++;
+        voiture_a_ajouter.Proprietaire = this ;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" + "nom=" + nom + ", prenom=" + prenom + '}';
+    }
+   
 }
--- TP2_relation_1_FOUREL/src/tp2_relation_1_fourel/Voiture.java
+++ TP2_relation_1_FOUREL/src/tp2_relation_1_fourel/Voiture.java
@@ -1,5 +1,6 @@
 package tp2_relation_1_fourel;
 
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +11,21 @@ package tp2_relation_1_fourel;
  * @author foure
  */
 public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+    Personne Proprietaire = null;
+
+    public Voiture(String nomModele, String nomMarque, int nbPuissanceCV) {
+        Modele = nomModele;
+        Marque = nomMarque;
+        PuissanceCV = nbPuissanceCV;
+        
+    }
     
+    @Override
+    public String toString() {
+        return "Voiture{" + "Modele=" + Modele + ", Marque=" + Marque + ", PuissanceCV=" + PuissanceCV + '}';
+    }
+ 
 }
